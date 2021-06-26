96500c13bcc29d27b2bf91d5f4e39fb09952dede
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2021-06-26 19:53:39 +0200

    Update MyDataRestConfig.java

--- MyDataRestConfig.java
+++ MyDataRestConfig.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
         //disable Http methods for Product: PUT, POST, DELETE
@@ -29,3 +29,6 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     }
 }
+
+
+
