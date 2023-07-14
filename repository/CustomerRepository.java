01a24c7acebf72bf7ba10b7ff136d81caf7c4641
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-07-14 15:34:11 +0300

    Delete repository directory

--- repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mahmoud.ecommerce.dao;
-
-import com.mahmoud.ecommerce.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
