e90495c479510e0523bd0f709158590d72a39e65
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-07-14 15:34:29 +0300

    Delete service directory

--- service/CheckoutService.java
+++
@@ -1,9 +0,0 @@
-package com.mahmoud.ecommerce.service;
-
-import com.mahmoud.ecommerce.dto.Purchase;
-import com.mahmoud.ecommerce.dto.PurchaseResponse;
-
-public interface CheckoutService {
-
-    PurchaseResponse placeOrder(Purchase purchase);
-}
